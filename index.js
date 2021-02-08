2b211826c71098a523580d049bc392b62f363449
lucas da silva costa <45472635+luccasscds@users.noreply.github.com>
2021-02-07 22:50:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-var data = new Date();
-var hora = data.getHours();
-console.log(hora);
-document.getElementById("time").innerHTML = hora;
+var momentoatual = new Date();
+
+/*data*/
+var dia = momentoatual.getDate();
+var mes = momentoatual.getMonth();
+var ano = momentoatual.getFullYear();
+
+/*hora*/
+var hora = momentoatual.getHours();
+var minuto = momentoatual.getMinutes();
+var segundo = momentoatual.getSeconds();
+
+/*funcoes*/
+function convertermes() {
+    let listameses = {0:'jan', 1:'fev', 2:'mar', 3:'abr', 4:'mai', 5:'jun',
+    6:'jul', 7:'ago', 8:'set', 9:'out', 10:'nov', 11:'dez'};
+
+    if(mes == 1){
+    mes = listameses[1];
+    }
+}
+
+function relogio() {
+    convertermes();
+    console.log(hora+':'+minuto+':'+segundo+'s');
+    console.log(dia+'/'+mes+'/'+ano);
+}
+relogio();
