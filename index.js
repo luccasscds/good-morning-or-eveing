1173a26489717df9211ffb55346dd111a8d7260a
lucas da silva costa <45472635+luccasscds@users.noreply.github.com>
2021-02-08 12:53:08 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
-var momentoatual = new Date();
+function main() {
+    var momentoatual = new Date();
 
-/*data*/
-var dia = momentoatual.getDate();
-var mes = momentoatual.getMonth();
-var ano = momentoatual.getFullYear();
+    /*data*/
+    var hoje = momentoatual.getDay();
+    var dia = momentoatual.getDate();
+    var mes = momentoatual.getMonth();
+    var ano = momentoatual.getFullYear();
 
-/*hora*/
-var hora = momentoatual.getHours();
-var minuto = momentoatual.getMinutes();
-var segundo = momentoatual.getSeconds();
+    /*hora*/
+    var hora = momentoatual.toLocaleTimeString();
 
-/*funcoes*/
-function convertermes() {
-    let listameses = {0:'jan', 1:'fev', 2:'mar', 3:'abr', 4:'mai', 5:'jun',
-    6:'jul', 7:'ago', 8:'set', 9:'out', 10:'nov', 11:'dez'};
+    /*funcoes*/
+    function convertermes() {
+        let listameses = { 0: 'janeiro', 1: 'fevereiro', 2: 'março', 3: 'abril', 4: 'maio', 5: 'junho', 6: 'julho', 7: 'agosto', 8: 'setembro', 9: 'outubro', 10: 'novembro', 11: 'dezembro' };
 
-    if(mes == 1){
-    mes = listameses[1];
+        for (i in listameses) {
+            if (mes == i) {
+                mes = listameses[i];
+            }
+        }
     }
-}
 
-function relogio() {
-    convertermes();
-    console.log(hora+':'+minuto+':'+segundo+'s');
-    console.log(dia+'/'+mes+'/'+ano);
+    function convertertoday() {
+        let listatoday = { 0: 'Domingo', 1: 'Segunda-feira', 2: 'Terça-feira', 3: 'Quarta-feira', 4: 'Quinta-feira', 5: 'Sexta-feira', 6: 'Sábado' }
+        for (i in listatoday) {
+            if (hoje == i) {
+                hoje = listatoday[i];
+            }
+        }
+    }
+
+    function relogio() {
+        document.getElementById('hora').textContent = hora;
+    }
+
+    function datas() {
+        convertertoday();
+        convertermes();
+        let dataAtual = hoje + ', ' + dia + ' de ' + mes + ' de ' + ano;
+        document.getElementById('data').textContent = dataAtual;
+
+    }
+    relogio();
+    datas();
 }
-relogio();
+main();
+setInterval(() => {
+    main();
+}, 1000);
